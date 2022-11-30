3b290d7e819d1e6fcb5f01aa11f774693db3aeec
GavrieliYovel <yovel.gavrieli@stu.shenkar.ac.il>
2022-11-30 16:55:06 +0200

    working postman server

--- routers/boardsAndTasksRouter.js
+++ routers/boardsAndTasksRouter.js
@@ -1,26 +1,13 @@
-// const { errorHandler,showAllBoards,getTasksByBoard,
-//     createNewTask, createNewBoard, deleteBoard,deleteTask,
-//     filterBoardByParameters, updateTask,updateBoard, exportBoardToCSV, renderHomePage } = require('./controller');
+const {Router} = require('express');
+const { boardController } = require('../controllers/boardsAndTasksController');
 
-const { Router } = require('express');
-const {boardsAndTasksController} = require('../controllers/boardsAndTasksController');
-const boardsAndTasksRouter = new Router() ;
-const boardJson = require('../res/tasks.json');
+const boardRouter = new Router();
 
+boardRouter.get('/boards', boardController.getBoards);  //
+boardRouter.get('/boards/:id', boardController.getTasksByBoard);       // localhost:8080/api/orders/6
+boardRouter.post('/boards', boardController.createNewBoard);         //
+boardRouter.put('/boards/:id', boardController.createNewTask);       //
+boardRouter.delete('/boards', boardController.deleteBoard);    //
+boardRouter.delete('/boards/:id', boardController.deleteTask);    //
 
-
-boardsAndTasksRouter.get('/',boardsAndTasksController.renderHomePage);
-boardsAndTasksRouter.get('/boards',boardsAndTasksController.showAllBoards);
-boardsAndTasksRouter.get('/boards/:BoardId',boardsAndTasksController.getTasksByBoard);
-boardsAndTasksRouter.post('/boards',boardsAndTasksController.createNewBoard);
-boardsAndTasksRouter.post('/boards/:id', boardsAndTasksController.createNewTask);
-boardsAndTasksRouter.post('/boards/:id/:filter',boardsAndTasksController.filterBoardByParameters);
-boardsAndTasksRouter.put('boards/:id',boardsAndTasksController.updateBoard);
-boardsAndTasksRouter.put('board/:id/tasks/:id',boardsAndTasksController.updateTask);
-boardsAndTasksRouter.delete('board/:id',boardsAndTasksController.deleteBoard);
-boardsAndTasksRouter.delete('board/:id/tasks/:id',boardsAndTasksController.deleteTask);
-
-
-
-
-module.exports = {boardsAndTasksRouter};
+module.exports = { boardRouter };
