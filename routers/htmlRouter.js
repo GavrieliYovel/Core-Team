d84f5ebc9fe733e852961fbe5c34c933990ba60f
GavrieliYovel <yovel.gavrieli@stu.shenkar.ac.il>
2022-12-07 16:30:38 +0200

    changing fetch to https://core-team.onrender.com

--- routers/htmlRouter.js
+++ routers/htmlRouter.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const { htmlController } = require('../controllers/htmlController');
 
 const htmlRouter = new Router();
-htmlRouter.get('/index', htmlController.getLogin);
+htmlRouter.get('/', htmlController.getLogin);
 htmlRouter.get('/boards', htmlController.getHome);
 htmlRouter.get('/tasks', htmlController.getTasksList);
 
